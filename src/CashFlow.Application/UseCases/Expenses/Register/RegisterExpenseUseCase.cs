055524d8667c9862c5c9a604b154b0b52c035e58
ArthurFloro <arthur.floro2006@gmail.com>
2026-06-08 23:19:44 -0300

    feat validando despesas usando if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -7,9 +8,36 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
         {
-            // TO DO VALIDATIONS
+            Validate(request);
 
             return new ResponseRegisterExpenseJson();
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if (request.Amount <= 0)
+            {
+                throw new ArgumentException("The amount must be greater than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if ( result > 0)
+            {
+                throw new ArgumentException("Expenses cannot be for the future.");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentsType), request.PaymentType);
+            if ( paymentTypeIsValid )
+            {
+                throw new ArgumentException("Payment Type id not valid");
+            }
+
         }
     }
 }
